efaa88899ac6285fe064f684453d37399359fa6b
Christopher Wood <cjwood032@gmail.com>
2020-02-07 19:29:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Write your solution here!
-let append =  ["Milo", "Otis", "Garfield"]
+//let append =  ["Milo", "Otis", "Garfield"]
 let pets =   ["Milo", "Otis", "Garfield"]
 //append.push("Odie")
 const append = (pets) =>{
-   pets.append("Odie")
+   pets.push("Odie")
   return pets
 }
